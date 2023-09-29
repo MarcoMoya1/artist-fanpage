c43605bd90c7194808593e42c6c768cd870d01e7
915marco <915marco@gmail.com>
2023-09-28 21:57:41 -0500

    playing around with js buttons

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-console.log("Lil Peep fan page");
+// console.log("Lil Peep fan page");
 
 const name = "Lil Peep";
 
 age = 21;
 
-alert(name + "was" +" " + age);
+// alert(name + "was" +" " + age);
+
+const btn = document.createElement('button');
+btn.innerText = 'Dont press this';
+document.body.appendChild(btn);
+
+btn.addEventListener('click', () => {
+    alert('Dont click that!');
+});
